8b112e70a32949cc86d35a26e3b69827fad03a1f
Andreas Ekström <didair@msn.com>
2023-03-05 04:01:02 +0100

    Add card component

---
+++ src/components/Card.js
@@ -0,0 +1,61 @@
+import cx from 'classnames';
+import useLongPress from 'lib/useLongPress';
+
+const Card = (props) => {
+
+	const onLongPress = () => {
+		if (props.onLongPress != null && typeof props.onLongPress == 'function') {
+			props.onLongPress();
+		}
+	};
+
+	const onClick = () => {
+		if (props.onClick != null && typeof props.onClick == 'function') {
+			props.onClick();
+		}
+	};
+
+	const options = {
+		shouldPreventDefault: true,
+		delay: 350,
+	};
+
+	const clickEvents = useLongPress(onLongPress, onClick, options);
+	const cleanProps = {...props};
+	delete cleanProps.onLongPress;
+	delete cleanProps.onClick;
+
+	return (
+		<div
+			{...cleanProps}
+			{...clickEvents}
+			className={cx(
+				'aspect-square',
+				'transition-all',
+				'ease-in-out',
+				'duration-200',
+				'flex',
+				'flex-col',
+				'justify-between',
+				'p-4',
+				'text-dark',
+				'rounded-lg',
+				'text-md',
+				'cursor-pointer',
+				'backdrop-blur-lg',
+				{
+					'bg-light': props.state == 'light',
+					'bg-light/20': props.state == 'dark',
+				},
+				...props.className
+			)}
+		/>
+	);
+};
+
+Card.defaultProps = {
+	className: '',
+	state: 'dark',
+};
+
+export default Card;
