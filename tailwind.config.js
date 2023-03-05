7f3dfdda2523a162265e3f73b1293abbdc38f06e
Andreas Ekström <didair@msn.com>
2023-03-05 02:01:23 +0100

    Add tailwind and postcss config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'dark': '#0d1113',
+      'light': '#ebeced',
+      'gray': '#5f6062',
+      'off-white': '#dce0e1',
+      'transparent': 'transparent',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
