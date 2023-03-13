329e1267f3e2269f6fed3246ca484f22cf29874f
Andreas Ekström <didair@msn.com>
2023-03-13 23:50:44 +0100

    Display message if rooms is empty

--- src/containers/pages/Devices.js
+++ src/containers/pages/Devices.js
@@ -1,9 +1,11 @@
 import { useGetConfigQuery } from "services/config/api";
 import { useGetStatesQuery } from "services/states/api";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 import Room from 'components/Room';
 import Masonry from "components/Masonry";
+import Icon from "components/Icon";
 
 const Devices = () => {
 	const { data: config } = useGetConfigQuery();
@@ -21,6 +23,15 @@ const Devices = () => {
 			: null}
 
 			<h3 className="text-xl mt-1 mb-4">Got states from {entities.length} devices</h3>
+
+			{rooms.length == 0 ?
+				<div className="my-40 flex items-center justify-center text-xl">
+					<span className="mr-2 text-xl">
+						<Icon name="circle-plus" />
+					</span>
+					Get started by adding a room in <Link to="/settings" className="font-bold ml-1 underline decoration-green">settings</Link>
+				</div>
+			: null}
 
 			<Masonry>
 				{rooms.map((room) => {
