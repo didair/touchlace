d2fd305628cb987aff7983de4c725b9b0ee4e7ca
Andreas Ekström <didair@msn.com>
2023-03-13 23:45:13 +0100

    Update Masonry grid

--- src/components/Masonry.js
+++ src/components/Masonry.js
@@ -1,13 +1,15 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useMemo } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import Isotope from "isotope-layout";
 
 const Masonry = (props) => {
 	const isotope = useRef();
-	const [filterKey, setFilterKey] = useState('*');
+	const container = useRef();
+	const id = useMemo(() => 'grid' + uuidv4().replaceAll('-', ''), []);
 
 	useEffect(() => {
-		isotope.current = new Isotope('.grid', {
-			itemSelector: '.card',
+		isotope.current = new Isotope(`#${id}`, {
+			itemSelector: `#${id} > .grid-item`,
 			layoutMode: 'masonry',
 			masonry: {
 				columnWidth: 160,
@@ -18,26 +20,18 @@ const Masonry = (props) => {
 		return () => isotope.current.destroy();
 	}, []);
 
-	useEffect(() => {
-		filterKey === '*'
-			? isotope.current.arrange({ filter: `*` })
-			: isotope.current.arrange({ filter: `.${filterKey}` })
-	}, [filterKey]);
+	// useEffect(() => {
+	// 	filterKey === '*'
+	// 		? isotope.current.arrange({ filter: `*` })
+	// 		: isotope.current.arrange({ filter: `.${filterKey}` })
+	// }, [filterKey]);
 
-	const handleFilterKeyChange = (key) => () => setFilterKey(key);
+	// const handleFilterKeyChange = (key) => () => setFilterKey(key);
 
 	return (
-		<>
-			<ul className="mb-4 flex gap-4">
-				<li onClick={handleFilterKeyChange('*')} className="cursor-pointer">Show all</li>
-				<li onClick={handleFilterKeyChange('switch')} className="cursor-pointer">Switches</li>
-				<li onClick={handleFilterKeyChange('light')} className="cursor-pointer">Lights</li>
-			</ul>
-
-			<div className="grid">
-				{props.children}
-			</div>
-		</>
+		<div ref={container} className="grid" id={id}>
+			{props.children}
+		</div>
 	);
 };
 
