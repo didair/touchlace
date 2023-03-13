f2a6adff97af077ff3319e3d236edd8c5117e891
Andreas Ekström <didair@msn.com>
2023-03-13 23:45:35 +0100

    Display rooms on Devices page

--- src/containers/pages/Devices.js
+++ src/containers/pages/Devices.js
@@ -1,12 +1,14 @@
 import { useGetConfigQuery } from "services/config/api";
 import { useGetStatesQuery } from "services/states/api";
+import { useSelector } from "react-redux";
 
-import Entity from "components/Entity";
+import Room from 'components/Room';
 import Masonry from "components/Masonry";
 
 const Devices = () => {
 	const { data: config } = useGetConfigQuery();
 	const { data: entities } = useGetStatesQuery();
+	const rooms = useSelector((state) => state.rooms.list);
 
 	if (entities == null) {
 		return null;
@@ -21,11 +23,13 @@ const Devices = () => {
 			<h3 className="text-xl mt-1 mb-4">Got states from {entities.length} devices</h3>
 
 			<Masonry>
-				{entities.map((entity) => {
-					return <Entity
-						key={entity.entity_id}
-						entity={entity}
-					/>;
+				{rooms.map((room) => {
+					return (
+						<Room
+							key={room.id}
+							{...room}
+						/>
+					);
 				})}
 			</Masonry>
 		</div>
