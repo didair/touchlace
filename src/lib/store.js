412c551031f520b0f24767d7bdf0cac6125ec0b9
Andreas Ekström <didair@msn.com>
2023-03-13 23:44:09 +0100

    Import roomsReducer

--- src/lib/store.js
+++ src/lib/store.js
@@ -4,17 +4,19 @@ import { persistReducer } from 'redux-persist';
 import { persistStore } from 'redux-persist'
 import storage from 'redux-persist/lib/storage';
 
+import roomsReducer from 'services/rooms/slice';
 import { configApi } from 'services/config/api';
 import { statesApi } from 'services/states/api';
 
 const persistConfig = {
 	key: 'root',
 	storage,
-	whitelist: ['config']
+	whitelist: ['config', 'rooms']
 };
 
 // Load reducers from services (or other)
 const reducers = combineReducers({
+	rooms: roomsReducer,
 	[configApi.reducerPath]: configApi.reducer,
 	[statesApi.reducerPath]: statesApi.reducer,
 });
