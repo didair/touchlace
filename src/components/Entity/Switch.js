771c67eb60ce8a5267117048df70489a1df3ac0f
Andreas Ekström <didair@msn.com>
2023-03-19 01:58:55 +0100

    Add entity modal for switches

--- src/components/Entity/Switch.js
+++ src/components/Entity/Switch.js
@@ -1,9 +1,12 @@
+import { useState } from 'react';
+import { capitalize } from 'lib/text';
 import cx from 'classnames';
 import Card from 'components/Card';
+import Modal from 'components/Modal';
 import Icon from 'components/Icon';
-import { capitalize } from 'lib/text';
 
 const EntitySwitch = ({ entity, updateState }) => {
+	const [open, setOpen] = useState(false);
 
 	const toggleOnOff = () => {
 		updateState({
@@ -14,29 +17,55 @@ const EntitySwitch = ({ entity, updateState }) => {
 	};
 
 	return (
-		<Card onClick={toggleOnOff} state={entity.state == 'on' ? 'light' : 'dark'} type="switch">
-			<div className="flex justify-between">
-				<div className={cx(
-					"h-11",
-					"flex",
-					"items-center",
-					"text-3xl",
-					{ 'text-gray': entity.state != 'on' }
-				)}>
-					<Icon name={entity.state == 'on' ? 'toggle-on' : 'toggle-off'} />
-				</div>
-			</div>
-
-			<div>
-				<div className="font-semibold truncate text-ellipsis h-6">
-					{entity.attributes.friendly_name}
+		<>
+			<Modal open={open} onClose={() => setOpen(false)}>
+				<div className="mb-4">
+					<h3 className="text-2xl mb-2">{entity.attributes.friendly_name}</h3>
+					<code className="block p-2 border border-gray/40 bg-gray/10 rounded-md">
+						{entity.entity_id}
+					</code>
 				</div>
 
-				<div className="font-semibold">
-					{capitalize(entity.state)}
+				<div className="mb-4">
+					<input
+						id="onOff"
+						type="checkbox"
+						checked={entity.state == 'on'}
+						onChange={toggleOnOff}
+					/>
+					<label htmlFor="onOff" className="ml-2">{capitalize(entity.state)}</label>
 				</div>
-			</div>
-		</Card>
+			</Modal>
+
+			<Card
+				onLongPress={() => setOpen(true)}
+				onClick={toggleOnOff}
+				state={entity.state == 'on' ? 'light' : 'dark'}
+				type="switch"
+			>
+				<div className="flex justify-between">
+					<div className={cx(
+						"h-11",
+						"flex",
+						"items-center",
+						"text-3xl",
+						{ 'text-gray': entity.state != 'on' }
+					)}>
+						<Icon name={entity.state == 'on' ? 'toggle-on' : 'toggle-off'} />
+					</div>
+				</div>
+
+				<div>
+					<div className="font-semibold truncate text-ellipsis h-6">
+						{entity.attributes.friendly_name}
+					</div>
+
+					<div className="font-semibold">
+						{capitalize(entity.state)}
+					</div>
+				</div>
+			</Card>
+		</>
 	);
 
 };
