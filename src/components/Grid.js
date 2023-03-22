aaf0bb536b926ed12c8c170809a35a36882d0ec4
Andreas Ekström <didair@msn.com>
2023-03-23 00:33:33 +0100

    Update breakpoints

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,7 +2,7 @@
 const Grid = (props) => {
 	return (
 		<div
-			className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-9"
+			className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-9"
 			{...props}
 		/>
 	);
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,13 @@ module.exports = {
       'blue': '#005bc5',
       'transparent': 'transparent',
     },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
   },
   plugins: [],
