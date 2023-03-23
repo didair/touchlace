923d707ceeb3694d21b961e7a6e7a6c3f7c4acd2
Andreas Ekström <didair@msn.com>
2023-03-23 01:34:59 +0100

    Add cog icon

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,35 +1,45 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
-const Icon = ({ name }) => {
+const Icon = ({ name, className }) => {
 	switch(name) {
 		case 'circle-plus':
-			return <FontAwesomeIcon icon={solid('circle-plus')} />
+			return <FontAwesomeIcon icon={solid('circle-plus')} className={className} />
 
 		case 'pen':
-			return <FontAwesomeIcon icon={solid('pen')} />
+			return <FontAwesomeIcon icon={solid('pen')} className={className} />
 
 		case 'trash-can':
-			return <FontAwesomeIcon icon={solid('trash-can')} />
+			return <FontAwesomeIcon icon={solid('trash-can')} className={className} />
 
 		case 'lightbulb':
-			return <FontAwesomeIcon icon={solid('lightbulb')} />
+			return <FontAwesomeIcon icon={solid('lightbulb')} className={className} />
 
 		case 'toggle-on':
-			return <FontAwesomeIcon icon={solid('toggle-on')} />
+			return <FontAwesomeIcon icon={solid('toggle-on')} className={className} />
 
 		case 'toggle-off':
-			return <FontAwesomeIcon icon={solid('toggle-off')} />
+			return <FontAwesomeIcon icon={solid('toggle-off')} className={className} />
 
 		case 'arrow-left':
-			return <FontAwesomeIcon icon={solid('arrow-left')} />
+			return <FontAwesomeIcon icon={solid('arrow-left')} className={className} />
 
 		case 'arrow-right':
-			return <FontAwesomeIcon icon={solid('arrow-right')} />
+			return <FontAwesomeIcon icon={solid('arrow-right')} className={className} />
+
+		case 'triangle-exclamation':
+			return <FontAwesomeIcon icon={solid('triangle-exclamation')} className={className} />
+
+		case 'gear':
+			return <FontAwesomeIcon icon={solid('gear')} className={className} />
 
 		default:
 			return null;
 	};
+};
+
+Icon.defaultProps = {
+	className: '',
 };
 
 export default Icon;
