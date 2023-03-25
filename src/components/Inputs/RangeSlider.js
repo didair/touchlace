fc10cf02312fdc2fc5fbddaaeec9ec2ef3769f81
Andreas Ekström <didair@msn.com>
2023-03-25 02:58:45 +0100

    Move debounce to rangeSlider and handle value internally in component

--- src/components/Inputs/RangeSlider.js
+++ src/components/Inputs/RangeSlider.js
@@ -1,24 +1,49 @@
+import { useId, useState, useMemo } from 'react';
+import debounce from 'lib/debounce';
 import './rangeSlider.css';
 
 const RangeSlider = (props) => {
-	const percentage = (parseInt(props.value) / parseInt(props.max)) * 100;
+	const [value, setValue] = useState(parseInt(props.value));
+	const percentage = (parseInt(value) / parseInt(props.max)) * 100;
+	const id = useId();
+
+	const updateParent = useMemo(() => debounce((event) => {
+		props.onChange(event);
+	}, 250), []);
+
+	const onChange = (event) => {
+		setValue(parseInt(event.target.value));
+		updateParent(event);
+	};
 
 	return (
-		<div style={{ height: 225 }}>
-			<input
-				type="range"
-				{...props}
-				className="slider"
-				style={{
-					backgroundImage: `linear-gradient(90deg, var(--bg-value-color) 0%, var(--bg-value-color) ${percentage}%, var(--bg-range-color) ${percentage}%, var(--bg-range-color) 100%)`
-				}}
-			/>
+		<div className="text-center">
+			<label htmlFor={id} className="block mb-4">
+				{Math.round(percentage) + '%'}
+			</label>
+
+			<div style={{ height: 225 }}>
+				<input
+					id={id}
+					min={props.min}
+					max={props.max}
+					value={value}
+					onChange={onChange}
+					type="range"
+					className="slider"
+					style={{
+						backgroundImage: `linear-gradient(90deg, var(--bg-value-color) 0%, var(--bg-value-color) ${percentage}%, var(--bg-range-color) ${percentage}%, var(--bg-range-color) 100%)`
+					}}
+				/>
+			</div>
 		</div>
 	);
 
 };
 
 RangeSlider.defaultProps = {
+	onChange: () => null,
+	value: '0',
 	min: '0',
 	max: '100',
 };
