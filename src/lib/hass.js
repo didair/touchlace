a4a1452bf3e1b38148544ba40480222787ca2299
Andreas Ekström <didair@msn.com>
2023-05-19 01:38:08 +0200

    Dont create new socket connection every time getHassConnection is called

--- src/lib/hass.js
+++ src/lib/hass.js
@@ -31,11 +31,16 @@ export const getHassAuth = async () => {
 	return auth;
 };
 
+var hass_session_connection = null;
 export const getHassConnection = async () => {
 	const auth = await getHassAuth();
 	if (auth != null) {
-		const connection = await createConnection({ auth });
-		return connection;
+		if (hass_session_connection != null) {
+			return hass_session_connection;
+		}
+
+		hass_session_connection = await createConnection({ auth });
+		return hass_session_connection;
 	}
 
 	return null;
