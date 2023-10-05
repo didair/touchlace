ef1dba516ad5799d19ba9f34ebc5595c8d4081d3
Andreas Ekström <didair@msn.com>
2023-10-06 01:44:28 +0200

    add getEntityType and getEntitySettings helper methods

--- src/lib/entity.ts
+++ src/lib/entity.ts
@@ -1,5 +1,14 @@
 import { Entity as EntityInterface } from "types";
-export const getEntityType = (entity: EntityInterface) => {
+import { store } from "./store";
+
+export const getEntityType = (entity: EntityInterface): 'light' | 'sensor' | 'binary_sensor' | 'switch' | 'cover' | 'media_player' => {
 	const entity_type: string = entity.entity_id.split('.')[0];
 	return entity_type;
+};
+
+export const getEntitySettings = (entity_id: string) => {
+	const state = store.getState();
+	return state.settings.entities.find((entity: EntityInterface) =>
+		entity.entity_id == entity_id
+	);
 };
