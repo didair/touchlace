80d6609f111d1497375bcb80f7e51f7eeabd0d37
Andreas Ekström <didair@msn.com>
2024-07-25 13:53:38 +0200

    Updates

--- src/components/Entity/Vacuum.tsx
+++ src/components/Entity/Vacuum.tsx
@@ -7,12 +7,13 @@ import { getBaseURI } from 'lib/config';
 import useInterval from 'lib/useInterval';
 import cx from 'classnames';
 
-import { useGetStatesQuery, useUpdateEntityStateMutation } from 'services/states/api';
+import { useCallEntityServiceMutation, useGetStatesQuery } from 'services/states/api';
 import { favoriteEntity } from 'services/settings/slice';
 
 import Card from 'components/Card';
 import Modal from 'components/Modal';
 import Icon from 'components/Icon';
+import Button from 'components/Inputs/Button';
 
 const EntityVacuum = ({
 	entity,
@@ -23,7 +24,7 @@ const EntityVacuum = ({
 }) => {
 	const dispatch = useDispatch();
 	const { data: entities } = useGetStatesQuery();
-	const [updateState] = useUpdateEntityStateMutation();
+	const [callService] = useCallEntityServiceMutation();
 	const [open, setOpen] = useState(false);
 	const [delay, setDelay] = useState(null);
 	const [mapPicture, setMapPicture] = useState(null);
@@ -31,11 +32,32 @@ const EntityVacuum = ({
 	const isFavorited = useSelector((state) => state.settings.favorites?.includes(entity.entity_id));
 	const vacuum: IVacuum = useSelector((state) => state.settings.vacuums.find((vac) => vac.id == entity.entity_id));
 
-	const toggleOnOff = () => {
-		updateState({
+	const startVacuum = () => {
+		callService({
 			entity_id: entity.entity_id,
-			domain: 'switch',
-			state: entity.state == 'on' ? 'off' : 'on',
+			domain: 'vacuum',
+			service: 'start',
+		});
+	};
+
+	const returnVacuum = () => {
+		callService({
+			entity_id: entity.entity_id,
+			domain: 'vacuum',
+			service: 'return_to_base',
+		});
+	};
+
+	const startRoomCleaning = (roomId) => {
+		callService({
+			entity_id: entity.entity_id,
+			domain: 'vacuum',
+			service: 'vacuum.send_command',
+			fields: {
+				command: 'app_segment_clean',
+				segments: [roomId],
+				repeat: 1,
+			},
 		});
 	};
 
@@ -60,6 +82,19 @@ const EntityVacuum = ({
 		return entities.find((entity) => entity.entity_id == vacuum.map);
 	}, [vacuum]);
 
+	const vacuumEntity: IEntity = useMemo(() => {
+		if (vacuum == null) {
+			return null;
+		}
+
+		return entities.find((entity) => entity.entity_id == vacuum.id);
+	}, [vacuum]);
+
+	const rooms: Array<object> = useMemo(() => {
+		const map = vacuumEntity.attributes.selected_map;
+		return vacuumEntity.attributes.rooms[map];
+	}, [vacuumEntity]);
+
 	useEffect(() => {
 		if (open) {
 			setMapPicture(getBaseURI() + mapEntity.attributes.entity_picture + '&_=' + Date.now());
@@ -68,6 +103,8 @@ const EntityVacuum = ({
 			setDelay(null);
 		}
 	}, [open]);
+
+	console.log('vacuum', vacuumEntity, rooms);
 
 	return (
 		<>
@@ -80,10 +117,67 @@ const EntityVacuum = ({
 					</h3>
 				</div>
 
-				<div>
-					{mapPicture != null ?
-						<img src={mapPicture} alt={vacuum.name + ' real time map'} />
-					: null}
+				<div className="flex my-8 gap-x-6">
+					<div className="w-9/12">
+						<div className="h-[60vh]">
+							{mapPicture != null ?
+								<img src={mapPicture} alt={vacuum.name + ' real time map'} className="h-full" />
+							: null}
+						</div>
+					</div>
+
+					<div className="w-3/12 flex justify-start items-center flex-col">
+						<div className={cx("bottom-3 w-1/2 flex items-center justify-center transition-all",
+							{
+								'text-light': entity.state == 'docked',
+								'animate-vacuum-running': entity.state == 'cleaning' || entity.state == 'paused',
+								'paused': entity.state == 'paused',
+								'animate-vacuum-returning': entity.state == 'returning'
+							}
+						)}>
+							<Icon name="vacuum" className="!w-full !h-full" />
+						</div>
+
+						<div className="my-4">
+							{capitalize(entity.state)}
+						</div>
+
+						<div className="w-full flex gap-2">
+							<div className="flex bg-green text-dark flex-col items-center flex-1 p-2 rounded-md cursor-pointer" onClick={startVacuum}>
+								<div>
+									<Icon name="broom" />
+								</div>
+
+								<div>
+									Start cleaning
+								</div>
+							</div>
+
+							<div className="flex bg-off-white text-dark flex-col items-center flex-1 p-2 rounded-md cursor-pointer" onClick={returnVacuum}>
+								<div>
+									<Icon name="RoutinesComingHome" />
+								</div>
+
+								<div>
+									Return to dock
+								</div>
+							</div>
+						</div>
+
+						<div className="w-full mt-4">
+							<strong>
+								Clean room
+							</strong>
+
+							<div className="flex flex-col gap-1">
+								{rooms?.map((room) => {
+									return <div className="flex bg-off-white text-dark flex-col flex-1 p-2 rounded-md cursor-pointer" onClick={() => startRoomCleaning(room.id)}>
+										{room.name}
+									</div>
+								})}
+							</div>
+						</div>
+					</div>
 				</div>
 
 				<div className="flex items-center justify-center">
@@ -95,8 +189,9 @@ const EntityVacuum = ({
 					</span>
 
 					<span
-						className={cx("ml-4 text-xl", { 'text-bright-green': isFavorited })}
+						className={cx("relative ml-4 text-xl", { 'text-bright-green': isFavorited })}
 						onClick={toggleFavorite}
+						style={{ top: '-2px' }}
 					>
 						<Icon name="star" />
 					</span>
@@ -112,7 +207,7 @@ const EntityVacuum = ({
 				state={entity.state != 'docked' ? 'light' : 'dark'}
 				type="vacuum"
 			>
-				<div className={cx("absolute left-0 bottom-3 w-full h-28 flex items-center justify-center",
+				<div className={cx("absolute left-0 bottom-3 w-full h-1/2 flex items-center justify-center transition-all",
 					{
 						'text-light': entity.state == 'docked',
 						'animate-vacuum-running': entity.state == 'cleaning' || entity.state == 'paused',
