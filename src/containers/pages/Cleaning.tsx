8259e1806e542442a60bc9444c3073f8ae9713f7
konandeks <konandeks@2020M014.local>
2024-10-07 21:26:46 +0200

    Make basic Cleaning page that lists all configured vacuums

--- src/containers/pages/Cleaning.tsx
+++ src/containers/pages/Cleaning.tsx
@@ -1,22 +1,44 @@
 import { useGetStatesQuery } from "services/states/api";
 import { useSelector } from "react-redux";
-import { IVacuum, IEntity } from "types";
+import { IEntity, IRoom } from "types";
+import { useMemo } from "react";
 
 import FoldersContainer from "components/FoldersContainer";
 import FolderContainer from "components/FolderContainer";
+import Entity from "components/Entity";
 
 const Cleaning = () => {
-	const { data: entities }: { data: [IEntity] } = useGetStatesQuery();
-	const vacuums: [IVacuum] = useSelector((state) => state.settings.vacuums);
+	const { data: entities }: { data?: IEntity[] } = useGetStatesQuery();
+	const rooms: IRoom[] = useSelector((state) => state.rooms.list);
 
-	console.log('vacuums', vacuums);
+	const roomsWithVacuums = useMemo(() => {
+		const found = rooms.filter((room) => {
+			return room.entities?.find((entity_id) => {
+				return entity_id.includes('vacuum.');
+			});
+		});
+
+		return found.map((room) => {
+			return {
+				...room,
+				entities: room.entities?.map((entity_id) => {
+					return entities?.find((entity) => entity.entity_id == entity_id);
+				}),
+			};
+		});
+	}, [rooms, entities]);
 
 	return (
 		<FoldersContainer>
-			{vacuums?.map((vacuum) => {
-				console.log('vacuum', vacuum);
+			{roomsWithVacuums?.map((room) => {
 				return (
-					<FolderContainer title={vacuum.name} key={vacuum.id}>
+					<FolderContainer title={room.name} key={room.id}>
+						{room.entities?.map((entity) => {
+							if (entity == null) return null;
+							return (
+								<Entity entity={entity} key={entity.entity_id} />
+							);
+						})}
 					</FolderContainer>
 				);
 			})}
